51e5bcfba4d7ebb0358de8b4e7b2d1f3ac87ddc0
Mariano <mmoranve@gmail.com>
2018-03-11 00:33:51 -0500

    fixing height
    transition still pending

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,20 @@ $.getJSON('data/Baruch subway entrances/all_stops_nyc_2017.geojson', function(su
 (function($) {
   "use strict"; // Start of use strict
 
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
