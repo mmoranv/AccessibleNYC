0fae0fda21a34380f439495e4830ed6aaf0a105c
Mariano <mmoranve@gmail.com>
2018-03-16 19:57:33 -0400

    getting the layers on

--- js/scripts.js
+++ js/scripts.js
@@ -1,121 +1,77 @@
 // *********************************** 1. Setting up the map************************************************
-	var defaultCenter = [40.701139, -74.010067];
-	var defaultZoom = 11;
+var defaultCenter = [40.701139, -74.010067];
+var defaultZoom = 11;
 
-	var map = L.map('subway-map').setView(defaultCenter, defaultZoom);
+var map = L.map('subway-map').setView(defaultCenter, defaultZoom);
 
-	// WorldGreyCanvas TileLayer from http://leaflet-extras.github.io/leaflet-providers/preview/
-	L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-		maxZoom: 15,
-	  scrollWheelZoom: false,
-	}).addTo(map);
+let ada_stops
+let nada_stops
+let all_stops
+let subway_lines
 
-	// Code to activate scroll wheel zoom after a click on the map PENDING REVISION
-	map.scrollWheelZoom.disable();
+// WorldGreyCanvas TileLayer from http://leaflet-extras.github.io/leaflet-providers/preview/
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+  maxZoom: 15,
+  scrollWheelZoom: false,
+}).addTo(map);
+
+// Code to activate scroll wheel zoom after a click on the map PENDING REVISION
+map.scrollWheelZoom.disable();
 
 // ********************************** 2. Setting up layers************************************************
-	//3 layers: Layer A: All stations, Accessible stations, non accessible stations
-	// Layer A: Color of the markers for All stations
-	function All_Stations(ADA) { //You can use this for categories too!
-		console.log(ADA)
-		return ADA === 1 ? '#008000' :
-					ADA === 0 ? '#BD0026' :
-							'#000000';
-	}
+//3 layers: Layer A: All stations, Accessible stations, non accessible stations
+// Layer A: Color of the markers for All stations
 
-	// Layer A: Getting the data for the All stations map layer
+$.getJSON('data/all_stops_nyc_2017.geojson', function(subways) {
+  all_stations = L.geoJSON(subways, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        color: "#004d99",
+        radius: 2
+      }).bindPopup(feature.properties.stop_name + " on the " + feature.properties.trains + " train.");
+    }
+  });
+});
 
-	// $.getJSON('data/all_stops_nyc_2017.geojson', function(subways) {
-	// 		 L.geoJSON(subways, {
-	// 			pointToLayer: function (feature,latlng) {
-	// 				return L.circleMarker (latlng, {
-	// 					// color: All_Stations(feature.properties.ADA),
-	// 					color: "green",
-	// 					radius: 2
-	// 				})
-	// 			}
-	//    	}).addTo(map)
-	// 	});
+// Function takes geojson object from '$.getJSON' and a char(2) landuse code
+// and returns a leaflet layer with
+// const getLandUseLayer = (mappluto, landUseCode) => {
+//   return L.geoJSON(mappluto, {
+//     filter: function(feature, layer) {
+//       feature.properties.land_use_17.indexOf(landUseCode)
+//     },
+//     style: propertyStyles,
+//     onEachFeature: propertyActions
+//   });
+// };
 
-	// Layer B: Accessible stations
+// Layer B: Accessible stations
 // var ADA_Layer =
-	$.getJSON('data/ada_stops_nyc_2017.geojson', function(subways) {
-			L.geoJSON(subways, {
-				pointToLayer: function (feature,latlng) {
-					return L.circleMarker (latlng, {
-						color: "#00cc66",
-						radius: 2
-					})
-				}
-			}).addTo(map);
-		});
-
-// 	var ADA_markers = {
-// 	 radius: 2,
-// 	 opacity: 1,
-//
-// 	 fillOpacity: 0.5,
-// 	 weight: .05,
-// 	};
-//
-// $.getJSON('data/accessible_stops_nyc_2017.geojson', function(stations) {
-// 	L.geoJSON(stations, {
-// 	 pointToLayer: function (feature, latlng) {
-// 	   var marker = L.circleMarker(latlng, ADA_markers)
-// 		 	.bindPopup(feature.properties.stop_name + ' running the ' +  feature.properties.trains + 'trains.', {offset: [0, -6]});
-// 	       marker.on('mouseover', function (e) {
-// 	           this.openPopup();
-// 	       });
-// 	       marker.on('mouseout', function (e) {
-// 	           this.closePopup();
-// 	       });
-// 	    return marker;
-// 	   }
-// 	}).addTo(map);
-// });
+$.getJSON('data/ada_stops_nyc_2017.geojson', function(subways) {
+  ada_stops = L.geoJSON(subways, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        color: "#00cc66",
+        radius: 2
+      }).bindPopup(feature.properties.stop_name + " station is accessible when using the " + feature.properties.trains + " train.");
+    }
+  }).addTo(map);
+});
 
 // //Layer C: Non accessible stations
 // var n_ADA_Layer =
 $.getJSON('data/n_accessible_stops_nyc_2017.geojson', function(subways) {
-		L.geoJSON(subways, {
-			pointToLayer: function (feature,latlng) {
-				return L.circleMarker (latlng, {
-					color: "red",
-					radius: 2
-				})
-			}
-		}).addTo(map);
-	});
+  nada_stops = L.geoJSON(subways, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        color: "red",
+        radius: 2
+      }).bindPopup(feature.properties.stop_name + " station is accessible when using the " + feature.properties.trains + " train.");
+    }
+  }).addTo(map);
+});
 
-// var RetailPoints = {
-//  radius: 10,
-//  opacity: 1,
-//  fillColor: "RED",
-//  fillOpacity: 0.5,
-//  weight: .05,
-// };
-//
-// var RetailOverlay = L.geoJSON(NewRetailFloorArea, {
-//  pointToLayer: function (feature, latlng) {
-//    var marker = L.circleMarker(latlng, RetailPoints)
-//        .bindPopup(feature.properties.Address + ' Built in ' +  feature.properties.YearBuilt, {offset: [0, -6]});
-//        marker.on('mouseover', function (e) {
-//            this.openPopup();
-//        });
-//        marker.on('mouseout', function (e) {
-//            this.closePopup();
-//        });
-//
-//     return marker;
-//    }
-// })
-
-// var layers = {
-// 	"Accessible stations": ADA_Layer,
-// 	"Non accessible stations": n_ADA_Layer,
-// };
-//
 // L.control.layers({}, layers).addTo(map);
 
 
